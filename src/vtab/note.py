b69e4f3869b48a2d564df0b706f96b1eceff066d
Daniel Thompson <daniel@redfelineninja.org.uk>
2013-06-22 22:07:02 +0100

    note: Provide a comparison operation.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/vtab/note.py
+++ src/vtab/note.py
@@ -15,6 +15,9 @@ class Note(object):
 		except:
 			self.set(s)
 			
+	def __cmp__(self, other):
+		return int(self) - int(other)
+
 	def __add__(self, other):
 		return Note(self.pitch + other)
 
@@ -69,7 +72,7 @@ class Note(object):
 			
 		self.pitch += self.MIDI_OFFSET + 12 * int(octave)
 	
-	def midi(self):
+	def __int__(self):
 		return self.pitch
 
 class NoteTest(unittest.TestCase):
@@ -83,14 +86,14 @@ class NoteTest(unittest.TestCase):
 		self.note = None
 
 	def testMatchMidiEnumeration(self):
-		self.assertEqual(self.note.midi(), 60)
+		self.assertEqual(int(self.note), 60)
 
 	def testSimpleRepr(self):
 		self.assertEqual(str(self.note), 'C4')
 
 	def testSimpleSharp(self):
 		self.note.set('C#4')
-		self.assertEqual(self.note.midi(), 61)
+		self.assertEqual(int(self.note), 61)
 
 	def testIdentityInKeyOfC(self):
 		for octave in range(0, 9):
@@ -110,16 +113,16 @@ class NoteTest(unittest.TestCase):
 		for octave in range(0, 9):
 			for note in self.NOTES_WITH_SHARPS:
 				self.note.set(note + str(octave))
-				self.assertFalse(self.note.midi() in d, "pitch already used")
-				d[self.note.midi()] = True
+				self.assertFalse(int(self.note) in d, "pitch already used")
+				d[int(self.note)] = True
 				
 	def testNoGapsInPitch(self):
 		last_pitch = 11 # MIDI represenation of B-1
 		for octave in range(0, 9):
 			for note in self.NOTES_WITH_SHARPS:
 				self.note.set(note + str(octave))
-				self.assertEqual(last_pitch+1, self.note.midi())
-				last_pitch = self.note.midi()
+				self.assertEqual(last_pitch+1, int(self.note))
+				last_pitch = int(self.note)
 				
 	def testAddition(self):
 		self.assertEqual('D4', str(self.note + 2))
@@ -134,6 +137,20 @@ class NoteTest(unittest.TestCase):
 	def testIncrementalSubtraction(self):
 		self.note -= 1
 		self.assertEqual('B3', str(self.note))
+
+	def testEqualityComparison(self):
+		localnote = Note(int(self.note))
+		self.assertEqual(localnote, self.note)
+
+	def testGreaterThan(self):
+		localnote = Note(int(self.note) + 1)
+		self.assertGreater(localnote, self.note)
+		self.assertFalse(self.note > localnote)
+
+	def testLessThan(self):
+		localnote = Note(int(self.note) - 1)
+		self.assertLess(localnote, self.note)
+		self.assertFalse(self.note < localnote)
 
 if __name__ == "__main__":
 	#import sys;sys.argv = ['', 'Test.testName']
