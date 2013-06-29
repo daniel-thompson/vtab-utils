4795187e26957f38842712165f106c3ff8bd4d7c
Daniel Thompson <daniel@redfelineninja.org.uk>
2013-06-29 18:26:21 +0100

    vtab: Follow PEP-8 ordering.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/vtab/__init__.py
+++ src/vtab/__init__.py
@@ -1,11 +1,13 @@
+from ascii_formatter import AsciiFormatter
+from dummy_formatter import DummyFormatter
+from ly_formatter import LilypondFormatter
+from note import Note
+from vtab_parser import VtabParser
+
 __all__ = [
 	'ascii_formatter',
 	'dummy_formatter',
 	'ly_formatter',
+	'note',
 	'vtab_parser'
 ]
-
-from ascii_formatter import AsciiFormatter
-from dummy_formatter import DummyFormatter
-from note import Note
-from vtab_parser import VtabParser
