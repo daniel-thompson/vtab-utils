15412dbbb8ffe0fa7e769c4cf92d39354f79e699
Daniel Thompson <daniel@redfelineninja.org.uk>
2013-10-27 09:05:15 +0000

    setup.py: Introduce basic integration with unittests.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import distutils.core
+import unittest
 
-setup(name='vtab-utils',
+class test(distutils.core.Command):
+	description = "run the test suite"
+
+	user_options = [
+	]
+
+	def initialize_options(self):
+		pass
+
+	def finalize_options(self):
+		pass
+
+	def run(self):
+		suite = unittest.defaultTestLoader.discover('tests')
+		runner = unittest.TextTestRunner()
+		result = runner.run(suite)
+		if not result.wasSuccessful():
+			raise distutils.errors.DistutilsError('Test suite failed')
+
+distutils.core.setup(name='vtab-utils',
       version='1.9.0',
       description='A collection of programs to work with vertical tab',
       author='Daniel Thompson',
       url='http://redfelineninja.org.uk/daniel/',
       packages=['vtab'],
       scripts=['vtab2ascii', 'vtab2dummy', 'vtab2ly', 'vtab2pdf'],
+      cmdclass={'test': test}
      )
